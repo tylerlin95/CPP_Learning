82f1061739713d578b521be94f8925c011ebdb10
lynn840429 <lynn840429@gmail.com>
2022-02-15 13:09:42 +0800

    [2022/02/15 lynn840429] Ver: 1.22.0; Info: Class 29; Todo: None

---
+++ C29/Main.cpp
@@ -0,0 +1,48 @@
+/**************************************************
+ * FileName: Main.cpp
+ * Usage: $ make
+ * Description: ...
+ * Dependency: ...
+ * Author: Lynn <lynn840429@gmail.com>
+ * Version: ...
+ **************************************************/
+/*** Include Packages ***/
+#include <iostream>
+#include <cstring>
+#include "log.h"
+
+
+
+/*** User Defined Parameters ***/
+#define LOG(x) std::cout << x << std::endl;
+
+
+
+/*** Folder/Path/Parameters Setting ***/
+
+
+
+/*** Main ***/
+int main()
+{
+    /***
+     * Process Info: ...
+     ***/
+    std::cout << "Hello World!" << std::endl;
+    
+    Entity *e = new Entity();
+    //std::cout << e->GetName() << std::endl;
+    //PrintName(e);
+    Print(e);
+
+    Player *p = new Player("Lynn");
+    //std::cout << p->GetName() << std::endl;
+    //PrintName(p);
+    Print(p);
+
+    Print(new A());
+    
+
+
+    return 0;
+}
--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.CLASS_FOLDER = ./C28/
+.CLASS_FOLDER = ./C29/
 
 hello : clean
 #	g++ $(.CLASS_FOLDER)Main.cpp $(.CLASS_FOLDER)log.h $(.CLASS_FOLDER)log.cpp $(.CLASS_FOLDER)static.cpp -o Main
